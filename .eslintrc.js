f939ad68054884524a245569863f593507e3cb57
Jose Garcia <joseg@axosoft.com>
2020-09-22 16:50:09 -0700

    add lint command + CI step

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "overrides": [
+    {
+      "files": ["**.ts", "**.tsx"],
+      "extends": [
+        "react-app",
+      ],
+      "rules": {
+        // "jsx-quotes": ["error", "prefer-single"]
+      },
+    },
+  ],
+};
